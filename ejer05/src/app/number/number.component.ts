7f9901203ea32f3cd406436e7e181fcdf75d9560
Jorge Arevalo <jorgeas80@gmail.com>
2016-09-29 23:41:10 +0200

    Alternative to using @Input and @Output decorators in ejer05

--- ejer05/src/app/number/number.component.ts
+++ ejer05/src/app/number/number.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-number',
   templateUrl: './number.component.html',
-  styleUrls: ['./number.component.css']
+  styleUrls: ['./number.component.css'],
+  inputs: ['n'],
+  outputs: ['show']
 })
 export class NumberComponent implements OnInit {
 
-  @Input() n = 0;
-  @Output() show = new EventEmitter<boolean>();
+  private n = 0;
+  private show = new EventEmitter<boolean>();
 
   private sw: boolean = false;
 
